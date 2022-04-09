3a945382e77bcdff8183e3ef97d169cb09d24320
bocsa <bocsardi.bogdan@gmail.com>
2022-04-09 17:13:58 +0300

    update web project

--- src/main/java/Model/User.java
+++ src/main/java/Model/User.java
@@ -1,4 +1,33 @@
 package Model;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.simple.JSONObject;
 
+import java.io.IOException;
 public class User {
+
+
+    private String username;
+    private String password;
+    public ObjectMapper objectMapper = new ObjectMapper();
+    public static final String TEST_DATA = System.getProperty("testData");
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+//    public User getTestData(String testData) throws IOException {
+//        JSONObject jsonObject = utils.JsonUtils.getTestData(testData);
+//        try {
+//            return objectMapper.readValue(jsonObject.toJSONString(), User.class);
+//        } catch (JsonProcessingException e){
+//            e.printStackTrace();
+//            throw e;
+//        }
+//    }
 }
--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,3 +1,4 @@
+import Model.User;
 import Screens.PracticeLogInTest;
 
 import org.openqa.selenium.WebDriver;
@@ -5,41 +6,24 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import utils.Driver;
+import utils.JsonUtils;
 
+@Listeners(utils.Listeners.class)
 public class BaseTest {
 
-    public static final String LINK= "https://www.saucedemo.com/";
-    public static final String BROWSER = System.getProperty("browser");
+    public static final String LINK = "https://www.saucedemo.com/";
     protected PracticeLogInTest practiceLogInTest;
+    protected User user;
+    protected String jsonUtils;
     private final Logger LOG = LoggerFactory.getLogger(BaseTest.class);
 
-    @BeforeMethod
+    @BeforeClass
     public void selectedBrowser() {
-        if (BROWSER.equals("chrome")) {
-            System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-            WebDriver driver = new ChromeDriver();
-            LOG.warn("Chrome is selected browser");
-            driver.navigate().to(LINK);
-            driver.manage().window().maximize();
-            practiceLogInTest = new PracticeLogInTest(driver);
-            try {
-                wait(5000);
-            } catch (InterruptedException | IllegalMonitorStateException e) {
-                e.printStackTrace();
-            }
-        } else if (BROWSER.equals("firefox")) {
-            System.setProperty("webdriver.gecko.driver", "src/main/resources/geckoDriver.exe");
-            WebDriver driver = new FirefoxDriver();
-            LOG.warn("Firefox is selected browser");
-            driver.navigate().to(LINK);
-            driver.manage().window().maximize();
-            practiceLogInTest = new PracticeLogInTest(driver);
-            try {
-                wait(5000);
-            } catch (InterruptedException | IllegalMonitorStateException e) {
-                e.printStackTrace();
-            }
-        }
+        Driver.Initialize();
+        Driver.Instance.navigate().to(LINK);
     }
 }
